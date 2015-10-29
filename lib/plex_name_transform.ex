875050ecfa86e102952dc5048328e1cc18da0741
Derrick McCray <derrickmccray@gmail.com>
2015-10-28 22:35:28 -0700

    working on extracting season

--- lib/plex_name_transform.ex
+++ lib/plex_name_transform.ex
@@ -4,11 +4,26 @@ defmodule PlexNameTransform do
 						|> String.strip								#strip whitespace
 						|> String.replace(" ", "(\\s|\\S)")   		#add regular expressions
 						|> add_parens								#add parenthesis around regular expression
+						|> String.upcase							#make string uppercase to match uppercase filename
 						|> Regex.compile							#create regular expression from string
 						|> elem(1)									#get the regex from the tuple
 
 		file = File.ls!(Path.absname(src_directory))
-		Enum.each(file, &(IO.puts("#{&1}: #{show} - #{String.match?(&1, show_regex)}")))
+		
+		{season, episode, seas_epi_tag} = get_season_episode(:series, file)
+
+		Enum.each(file, &(IO.puts("#{&1}: #{show} - #{String.match?(String.upcase(&1), show_regex)}")))
+
+
+
+		#case if match
+			#get and translate season
+			#find destination folder for show & season
+			#is file or folder
+			#cd inside folder if folder
+			#copy file
+		#else
+			#write to log no destination folder
 
 		#need to deal with season number as well
 		#need to call a new function to check if file or directory, if regex matched, get file (either from directory or file itself), and copy to destination folder
@@ -16,33 +31,39 @@ defmodule PlexNameTransform do
 	end
 
 	def match_content(:movie, src_directory, dest_directory, show) do
+		#tab to start automating movies
 	end
 
 	defp series?(src_path) do
 		Regex.run(~r/S\d\dE\d\d/, src_path) != nil
 	end
 
+	defp match_show(file, show) do
+
+	end
+
 	defp movie?(src_path) do
 		Regex.run(~r/\(\d\d\d\d\)/, src_path) != nil
 	end
 
-	defp create_destination(:series, src_path, dest_path) do
+	defp find_destination(:series, src_path, dest_path) do
 		
-		Enum.each(file, &(IO.puts(&1)))
-
 	end
 
-	defp create_destination(:movie, src_path) do
-	end	
+	defp get_season_episode(:series, file) do
+		[se_tag | tl] = Regex.run(~r/S\d\dE\d\d/, file)
+		[hd | tl] = String.split(se_tag, ["S","E"])
+		[car | [caar| cdr]] = tl
 
-	defp match_regex(:series, file_name, show, regex) do
+		{"Season #{String.lstrip(car,?0)}","Episode #{String.lstrip(caar,?0)}", se_tag}
+	end
 
+	defp exists?(:series, show, season) do
 	end
 
 	defp add_parens(str) do
 		"(" <> str <> ")"
 	end
-
 
 end
 
