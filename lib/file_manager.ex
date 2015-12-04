543de0580224da2c3b8a223730560ca1608d95de
Derrick McCray <derrickmccray@gmail.com>
2015-12-03 22:39:19 -0800

    added some logic to skip args and test directly in iex with iex -S mix

--- lib/file_manager.ex
+++ lib/file_manager.ex
@@ -26,7 +26,7 @@ defmodule FileManager do
 			{:done, {from, file_hash}} ->
 				#kill process
 				Process.exit(from, :kill)
-				IO.puts("Killing process: #{file_hash}")
+				IO.puts("Killing process: #{from} - #{file_hash}")
 				manage(process_count - 1)
 		end
 	end
